f3e3f83e1667420d2f862e1caf74d99c2306d3b5
zendaokaofficial <110347285+zendaokaofficial@users.noreply.github.com>
2023-07-28 14:05:43 +0800

    Update form-groundcheck.py

--- form-groundcheck.py
+++ form-groundcheck.py
@@ -33,6 +33,7 @@ df = pd.read_csv(url_1, header=0, on_bad_lines='skip')
 hari = date.today()
 
 if __name__ == "__main__":
+    st.title("🌱 Simantab")
     st.markdown("<h1 style='text-align: center; color: green;'>Form Pelaporan Groundcheck</h1>", unsafe_allow_html=True)
     #st.subheader(f"Tanggal: {hari}")
     #st.dataframe(df)
